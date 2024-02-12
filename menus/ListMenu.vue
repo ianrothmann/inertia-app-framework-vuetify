db012f950e71df0b5fa5e2fd7cf35513d8db298d
Werner Gerber <werner@greenbit.dev>
2024-02-12 22:13:55 +0200

    Updated menu styles

--- menus/ListMenu.vue
+++ menus/ListMenu.vue
@@ -1,18 +1,24 @@
 <template>
-  <v-list v-model:opened="openGroups" nav density="compact">
+  <v-list v-model:opened="openGroups" nav  :lines="false">
     <slot name="top"></slot>
-
-    <template v-for="(menuItem, itemIndex) in items">
-
-      <template v-if="!menuItem.items || menuItem.items.length ===0">
-        <v-list-item :prepend-icon="menuItem.icon" :title="menuItem.label" link :value="getKey(menuItem)"
-                     :active="currentRoute() === getKey(menuItem)"
-                     color="primary"
-                     @click.stop="navigate(menuItem)">
+    <template v-for="(menuItem, itemIndex) in items" :key="itemIndex">
+      <template v-if="!menuItem.items || menuItem.items.length === 0">
+        <v-list-item
+            :prepend-icon="menuItem.icon"
+            link
+            :value="getKey(menuItem)"
+            :active="currentRoute() === getKey(menuItem)"
+            color="primary"
+            @click.stop="navigate(menuItem)"
+        >
+          <v-list-item-title v-text="menuItem.label"></v-list-item-title>
+          <template v-if="menuItem.options&&menuItem.options.badge" v-slot:append>
+            <v-badge :content="menuItem.options.badge" color="red" inline bordered></v-badge>
+          </template>
         </v-list-item>
       </template>
 
-      <v-list-group v-if="menuItem.items && menuItem.items.length > 0" color="primary" :value="getKey(menuItem)">
+      <v-list-group v-else color="primary" :value="getKey(menuItem)">
         <template v-slot:activator="{ props }">
           <v-list-item
               v-bind="props"
@@ -23,6 +29,7 @@
         <v-list-item
             class="sub-item"
             v-for="(subItem, subIndex) in menuItem.items"
+            :key="subIndex"
             :title="subItem.label"
             :prepend-icon="subItem.icon"
             :value="getKey(subItem)"
@@ -30,9 +37,14 @@
             :active="currentRoute() === getKey(subItem)"
             color="primary"
             link
-        ></v-list-item>
+        >
+          <template v-if="subItem.options&&subItem.options.badge" v-slot:append>
+            <v-badge :content="subItem.options.badge" color="red" overlap inline bordered></v-badge>
+          </template>
+        </v-list-item>
       </v-list-group>
     </template>
+
     <slot></slot>
   </v-list>
 </template>
@@ -42,17 +54,15 @@ import MenuComponentMixin from './MenuComponentMixin';
 import menu from "./Menu.vue";
 
 export default {
-  computed: {
-    menu() {
-      return menu
-    }
-  },
-  props: {
-    state: {
-      type: String,
-    },
-  },
   mixins: [MenuComponentMixin],
+  props: {
+    state: String,
+  },
+  data() {
+    return {
+      menu: menu,
+    };
+  },
   mounted() {
     this.setOpenGroups();
   },
